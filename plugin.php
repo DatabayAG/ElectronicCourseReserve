948741db9a332fa8516e38b6cc1da6c40b633b30
Maja Boldt <mboldt@databay.de>
2024-06-19 14:14:57 +0200

    adapts required ilias and php version

--- plugin.php
+++ plugin.php
@@ -7,8 +7,8 @@ $version = '3.0.0';
 
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
-$ilias_min_version = '7.0';
-$ilias_max_version = '7.999';
+$ilias_min_version = '9.0';
+$ilias_max_version = '9.999';
 
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = 'Guido Vollbach / Michael Jansen / Nadia Matuschek';
