eb76b7dd5cf06a673842613d32a68cb41cae49d0
mjansen <mjansen@databay.de>
2024-08-28 09:55:39 +0200

    Fix "bib" library button view modifier

--- classes/modifier/class.ilECRBibliographicItemModifier.php
+++ classes/modifier/class.ilECRBibliographicItemModifier.php
@@ -1,18 +1,28 @@
 <?php
 
-/* Copyright (c) 1998-2018 ILIAS open source, Extended GPL, see docs/LICENSE */
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
+
+declare(strict_types=1);
 
 use ILIAS\HTTP\Wrapper\WrapperFactory;
 use ILIAS\Plugin\ElectronicCourseReserve\Library\LinkBuilder;
 use ILIAS\Plugin\ElectronicCourseReserve\Objects\Helper;
 use ILIAS\Refinery\Factory;
 
-require_once 'Customizing/global/plugins/Services/UIComponent/UserInterfaceHook/ElectronicCourseReserve/classes/interfaces/interface.ilECRBaseModifier.php';
-
-/**
- * Class ilECRBibliographicItemModifier
- * @author Michael Jansen <mjansen@databay.de>
- */
 class ilECRBibliographicItemModifier implements ilECRBaseModifier
 {
     private WrapperFactory $httpWrapper;
@@ -24,12 +34,20 @@ class ilECRBibliographicItemModifier implements ilECRBaseModifier
         $this->httpWrapper = $DIC->http()->wrapper();
         $this->refinery = $DIC->refinery();
     }
-    /**
-     * @inheritdoc
-     * @throws ilException
-     */
+
     public function shouldModifyHtml($a_comp, $a_part, $a_par): bool
     {
+        $refId = $this->httpWrapper->query()->retrieve(
+            'ref_id',
+            $this->refinery->byTrying([
+                $this->refinery->kindlyTo()->int(),
+                $this->refinery->always(0)
+            ])
+        );
+        if (!$refId) {
+            return false;
+        }
+
         if (!$this->isListView() && !$this->isDetailView()) {
             return false;
         }
@@ -38,16 +56,7 @@ class ilECRBibliographicItemModifier implements ilECRBaseModifier
             return false;
         }
 
-        if ($this->isDetailView() && $a_par['tpl_id'] !== 'Services/Form/tpl.form.html') {
-            return false;
-        }
-
-        if(!$this->httpWrapper->query()->has('ref_id')) {
-            return false;
-        }
-        $refId = $this->httpWrapper->query()->retrieve('ref_id', $this->refinery->kindlyTo()->int());
-
-        if (!$refId) {
+        if ($this->isDetailView() && $a_par['tpl_id'] !== 'src/UI/templates/default/Panel/tpl.sub.html') {
             return false;
         }
 
@@ -57,7 +66,6 @@ class ilECRBibliographicItemModifier implements ilECRBaseModifier
 
         /** @var Helper $objectHelper */
         $objectHelper = $GLOBALS['DIC']['plugin.esa.object.helper'];
-
         $instance = $objectHelper->getInstanceByRefId($refId);
         if ($instance->getType() !== 'bibl') {
             return false;
@@ -66,34 +74,37 @@ class ilECRBibliographicItemModifier implements ilECRBaseModifier
         return true;
     }
 
-    /**
-     * @return bool
-     */
     protected function isDetailView(): bool
     {
-        if(!$this->httpWrapper->query()->has('cmdClass') || !$this->httpWrapper->query()->retrieve('cmdClass', $this->refinery->kindlyTo()->string())) {
+        if (!$this->httpWrapper->query()->has('cmdClass') ||
+            !$this->httpWrapper->query()->retrieve('cmdClass', $this->refinery->kindlyTo()->string())) {
             return false;
         }
 
         return (
-            strtolower($this->httpWrapper->query()->retrieve('cmdClass', $this->refinery->kindlyTo()->string())) === strtolower(ilObjBibliographicGUI::class) &&
-            strtolower($this->httpWrapper->query()->retrieve('cmd', $this->refinery->kindlyTo()->string())) === 'showdetails'
+            strtolower(
+                $this->httpWrapper->query()->retrieve('cmdClass', $this->refinery->kindlyTo()->string())
+            ) === strtolower(ilObjBibliographicGUI::class) &&
+            strtolower(
+                $this->httpWrapper->query()->retrieve('cmd', $this->refinery->kindlyTo()->string())
+            ) === 'showdetails'
         );
     }
 
-    /**
-     * @return bool
-     */
     protected function isListView(): bool
     {
-        if(!$this->httpWrapper->query()->has('cmdClass') || !$this->httpWrapper->query()->has('cmd')) {
+        // TODO: Links like http://localhost.php8-1/ilias/9x/ilias.php?baseClass=ilrepositorygui&cmd=render&ref_id=148 should be considred as LIST VIEW context
+        if (!$this->httpWrapper->query()->has('cmdClass') || !$this->httpWrapper->query()->has('cmd')) {
             return false;
         }
+
         $cmdClass = $this->httpWrapper->query()->retrieve('cmdClass', $this->refinery->kindlyTo()->string());
         $cmd = $this->httpWrapper->query()->retrieve('cmd', $this->refinery->kindlyTo()->string());
-        if(!$cmdClass) {
+
+        if (!$cmdClass) {
             return false;
         }
+
         return (
             strtolower($cmdClass) === strtolower(ilObjBibliographicGUI::class) &&
             in_array(
@@ -109,32 +120,36 @@ class ilECRBibliographicItemModifier implements ilECRBaseModifier
         );
     }
 
-    /**
-     * @param ilObjCourse $crs
-     * @param array $a_par
-     * @return array
-     */
     protected function manipulateListView(ilObjCourse $crs, array $a_par): array
     {
-
+        /** @var list<ilBiblLibrary> $libs */
         $libs = ilBiblLibrary::get();
-        $libsShownInList = array_filter($libs, function (ilBiblLibrary $lib) {
+        $libsShownInList = array_filter($libs, static function (ilBiblLibrary $lib) {
             return $lib->isShownInList();
         });
 
-
-        if (0 === count($libsShownInList)) {
+        if (count($libsShownInList) === 0) {
             return ['mode' => ilUIHookPluginGUI::KEEP, 'html' => ''];
         }
 
         $dom = new DOMDocument("1.0", "utf-8");
         $dom->preserveWhiteSpace = true;
         $dom->formatOutput = true;
-        if (!@$dom->loadHTML('<!DOCTYPE html><html><head><meta charset="utf-8"/></head><body>' . $a_par['html'] . '</body></html>')) {
+        if (!@$dom->loadHTML(
+            '<!DOCTYPE html><html><head><meta charset="utf-8"/></head><body>' . $a_par['html'] . '</body></html>'
+        )) {
             return ['mode' => ilUIHookPluginGUI::KEEP, 'html' => ''];
         }
 
-        $table = $dom->getElementById('tbl_bibl_overview');
+        $table = $dom->getElementById(
+            'tbl_bibl_overview_' . $this->httpWrapper->query()->retrieve(
+                'ref_id',
+                $this->refinery->kindlyTo()->int()
+            )
+        );
+        if (!$table) {
+            return ['mode' => ilUIHookPluginGUI::KEEP, 'html' => ''];
+        }
 
         $xp = new DOMXPath($dom);
         $actionCells = $xp->query('tbody/tr/td[last()]', $table);
@@ -147,19 +162,19 @@ class ilECRBibliographicItemModifier implements ilECRBaseModifier
         $linkParameters = $linkBuilder->getLibraryUrlParameters($crs);
 
         foreach ($actionCells as $actionCell) {
-            /** @var $actionCell DOMNode */
-
-            $bibButtons = $xp->query('a', $actionCell);
+            /** @var DOMElement $actionCell */
+            $bibButtons = $xp->query('button[@data-action]', $actionCell);
             if ($bibButtons->length < 1) {
                 continue;
             }
 
             foreach ($bibButtons as $bibButton) {
-                $href = $bibButton->getAttribute('href');
+                /** @var DOMElement $bibButton */
+                $href = $bibButton->getAttribute('data-action');
                 foreach ($linkParameters as $paramKey => $paramValue) {
                     $href = ilUtil::appendUrlParameterString($href, $paramKey . '=' . $paramValue);
                 }
-                $bibButton->setAttribute('href', $href);
+                $bibButton->setAttribute('data-action', $href);
             }
         }
 
@@ -174,23 +189,19 @@ class ilECRBibliographicItemModifier implements ilECRBaseModifier
         ];
     }
 
-    /**
-     * @param ilObjCourse $crs
-     * @param array $a_par
-     * @return array
-     */
     protected function manipulateDetailView(ilObjCourse $crs, array $a_par): array
     {
         $dom = new DOMDocument("1.0", "utf-8");
         $dom->preserveWhiteSpace = true;
         $dom->formatOutput = true;
-        if (!@$dom->loadHTML('<!DOCTYPE html><html><head><meta charset="utf-8"/></head><body>' . $a_par['html'] . '</body></html>')) {
+        if (!@$dom->loadHTML(
+            '<!DOCTYPE html><html><head><meta charset="utf-8"/></head><body>' . $a_par['html'] . '</body></html>'
+        )) {
             return ['mode' => ilUIHookPluginGUI::KEEP, 'html' => ''];
         }
 
-        $form = $dom->getElementById('form_');
         $xp = new DOMXPath($dom);
-        $bibButtons = $xp->query('div/div/div/a[@href]', $form);
+        $bibButtons = $xp->query("//div[contains(concat(' ', normalize-space(@class), ' '), ' il-listing-characteristic-value-item ')]//button[@data-action]");
         if ($bibButtons->length <= 0) {
             return ['mode' => ilUIHookPluginGUI::KEEP, 'html' => ''];
         }
@@ -200,11 +211,12 @@ class ilECRBibliographicItemModifier implements ilECRBaseModifier
         $linkParameters = $linkBuilder->getLibraryUrlParameters($crs);
 
         foreach ($bibButtons as $bibButton) {
-            $href = $bibButton->getAttribute('href');
+            /** @var DOMElement $bibButton */
+            $href = $bibButton->getAttribute('data-action');
             foreach ($linkParameters as $paramKey => $paramValue) {
                 $href = ilUtil::appendUrlParameterString($href, $paramKey . '=' . $paramValue);
             }
-            $bibButton->setAttribute('href', $href);
+            $bibButton->setAttribute('data-action', $href);
         }
 
         $processedHtml = $dom->saveHTML($dom->getElementsByTagName('body')->item(0));
@@ -218,10 +230,6 @@ class ilECRBibliographicItemModifier implements ilECRBaseModifier
         ];
     }
 
-    /**
-     * @inheritdoc
-     * @throws ilException
-     */
     public function modifyHtml($a_comp, $a_part, $a_par): array
     {
         global $DIC;
@@ -229,15 +237,16 @@ class ilECRBibliographicItemModifier implements ilECRBaseModifier
         /** @var Helper $objectHelper */
         $objectHelper = $GLOBALS['DIC']['plugin.esa.object.helper'];
 
-        $instance = $objectHelper->getInstanceByRefId($this->httpWrapper->query()->retrieve('ref_id', $this->refinery->kindlyTo()->int()));
+        $instance = $objectHelper->getInstanceByRefId(
+            $this->httpWrapper->query()->retrieve('ref_id', $this->refinery->kindlyTo()->int())
+        );
         $parentCrsRefId = $DIC->repositoryTree()->checkForParentType($instance->getRefId(), 'crs', true);
         if (!$parentCrsRefId) {
             return ['mode' => ilUIHookPluginGUI::KEEP, 'html' => ''];
         }
 
         $crs = $objectHelper->getInstanceByRefId($parentCrsRefId);
-        if (
-            !($crs instanceof ilObjCourse) ||
+        if (!($crs instanceof ilObjCourse) ||
             !$DIC->access()->checkAccess('write', '', $crs->getRefId()) ||
             !ilElectronicCourseReservePlugin::getInstance()->isAssignedToRequiredRole($DIC->user()->getId())) {
             return ['mode' => ilUIHookPluginGUI::KEEP, 'html' => ''];
@@ -245,10 +254,8 @@ class ilECRBibliographicItemModifier implements ilECRBaseModifier
 
         if ($this->isListView()) {
             return $this->manipulateListView($crs, $a_par);
-        } else {
-            if ($this->isDetailView()) {
-                return $this->manipulateDetailView($crs, $a_par);
-            }
+        } elseif ($this->isDetailView()) {
+            return $this->manipulateDetailView($crs, $a_par);
         }
 
         return ['mode' => ilUIHookPluginGUI::KEEP, 'html' => ''];
