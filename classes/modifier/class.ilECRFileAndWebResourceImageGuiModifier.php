9bbe1eb2980a92ae55bf00bf5c6e613d65c78ebf
mjansen <mjansen@databay.de>
2024-08-28 08:51:06 +0200

    Fix issues in "file" and "webr" GUI view modifier

--- classes/modifier/class.ilECRFileAndWebResourceImageGuiModifier.php
+++ classes/modifier/class.ilECRFileAndWebResourceImageGuiModifier.php
@@ -1,24 +1,32 @@
 <?php
 
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
+
+declare(strict_types=1);
+
 use ILIAS\HTTP\Wrapper\WrapperFactory;
 use ILIAS\Refinery\Factory;
 
-require_once "Customizing/global/plugins/Services/UIComponent/UserInterfaceHook/ElectronicCourseReserve/classes/interfaces/interface.ilECRBaseModifier.php";
-require_once "Customizing/global/plugins/Services/UIComponent/UserInterfaceHook/ElectronicCourseReserve/classes/class.ilElectronicCourseReserveListGUIHelper.php";
-
-/**
- * Class ilECRFileAndWebResourceImageGuiModifier
- */
 class ilECRFileAndWebResourceImageGuiModifier implements ilECRBaseModifier
 {
-    protected array $object_types = array('file', 'webr');
-
+     /** @var list<string> */
+    protected array $object_types = ['file', 'webr'];
     protected ilObjectDataCache $data_cache;
-
-
     protected ilAccessHandler $access;
-
-
     protected bool $modified = false;
     private WrapperFactory $httpWrapper;
     private Factory $refinery;
@@ -32,19 +40,13 @@ class ilECRFileAndWebResourceImageGuiModifier implements ilECRBaseModifier
         $this->refinery = $DIC->refinery();
     }
 
-    /**
-     * @param $a_comp
-     * @param $a_part
-     * @param $a_par
-     * @return bool
-     */
     public function shouldModifyHtml($a_comp, $a_part, $a_par): bool
     {
         if ($this->modified) {
             return false;
         }
 
-        if(!$this->httpWrapper->query()->has('ref_id')) {
+        if (!$this->httpWrapper->query()->has('ref_id')) {
             return false;
         }
         $refId = $this->httpWrapper->query()->retrieve('ref_id', $this->refinery->kindlyTo()->int());
@@ -55,30 +57,25 @@ class ilECRFileAndWebResourceImageGuiModifier implements ilECRBaseModifier
         $obj_id = $this->data_cache->lookupObjId($refId);
         $type = $this->data_cache->lookupType($obj_id);
 
-        if (in_array($type, $this->object_types)) {
+        if (in_array($type, $this->object_types, true)) {
             $this->modified = true;
             return true;
         }
+
         return false;
     }
 
-    /**
-     * @param $a_comp
-     * @param $a_part
-     * @param $a_par
-     */
     public function modifyHtml($a_comp, $a_part, $a_par): array
     {
         global $DIC;
-        $plugin = ilElectronicCourseReservePlugin::getInstance();
+
         $refId = $this->httpWrapper->query()->retrieve('ref_id', $this->refinery->kindlyTo()->int());
-        ;
+
+        $plugin = ilElectronicCourseReservePlugin::getInstance();
         $item_data = $plugin->queryItemData($refId);
-        if (is_array($item_data)
-            && array_key_exists('icon', $item_data)
-            && strlen($item_data['icon']) > 0
-            && array_key_exists('show_image', $item_data)
-            && $item_data['show_image'] == 1) {
+        if (is_array($item_data) &&
+            array_key_exists('icon', $item_data) && is_string($item_data['icon']) && $item_data['icon'] !== '' &&
+            array_key_exists('show_image', $item_data) && is_numeric($item_data['show_image']) && (int) $item_data['show_image'] === 1) {
 
             $replace = '#headerimage';
             if (array_key_exists(
@@ -93,6 +90,10 @@ class ilECRFileAndWebResourceImageGuiModifier implements ilECRBaseModifier
             $DIC->ui()->mainTemplate()->addJavaScript('Customizing/global/plugins/Services/UIComponent/UserInterfaceHook/ElectronicCourseReserve/js/ElectronicCourseReserveObjectIcon.js');
             $DIC->ui()->mainTemplate()->addOnLoadCode('il.ElectronicCourseReserveObjectIcon.setConfig("' . $replace . '", "' . $with . '");');
         }
-        return [];
+
+        return [
+            'mode' => ilUIHookPluginGUI::KEEP,
+            'html' => ''
+        ];
     }
 }
